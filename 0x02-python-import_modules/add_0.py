e678646e23d0b3629b519a44c9391d9093be74c3
Nixon <nixusmwas@gmail.com>
2024-09-17 16:13:30 +0300

    using the add import

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """ my addition function
+    args:
+        a: the first integer
+        b: the second integr
+    Returns:
+        the sum of a + b
+    """
+    return (a + b)
