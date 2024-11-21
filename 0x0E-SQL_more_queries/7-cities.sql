78bedc69568b584f7275e8b64f10baea548cfbd7
Nixon <nixusmwas@gmail.com>
2024-11-22 01:38:55 +0300

    creating more databases with tables

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- script creates the database hbtn_0d_usa
+-- script creates the table cities in the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Switch to database
+USE hbtn_0d_usa;
+-- Creates a table within the database
+CREATE TABLE IF NOT EXISTS cities (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
+	);
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+-- script lists all the cities of california in hbtn_0d_usa
+SELECT id, name FROM cities WHERE state_id = 1 ORDER BY id ASC;
