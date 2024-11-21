9734a567ff82c72b275f7d6df17799413dd97e87
Nixon <nixusmwas@gmail.com>
2024-11-21 20:15:57 +0300

    delete database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- script creates a database
-CREATE DATABASE `hbtn_0c_0`;
+-- script creates a database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- script deletes a created database if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
+
